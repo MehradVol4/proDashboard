2bb7d157a456e659696d1feada1483c1ac1feaa4
Mehrad Khademi <mehradkhd.vol4@gmail.com>
2026-05-12 16:49:34 +0330

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
-import Input from "./ui/Input" ;
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size:30px ;
-  font-weight : 600;
-  background-color : yellow ;
-` ;
+
 
 
 const StyledApp = styled.div`
-  background-color : orangered ;
   padding : 20px ;
 
 ` ;
@@ -19,13 +16,26 @@ const StyledApp = styled.div`
 function App() {
   return (
     <>
+
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button onClick={() => alert('check in')}>Check in</Button>
-        <Button onClick={() => alert('check out')}>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('check in')}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={() => alert('check out')}>Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   )
